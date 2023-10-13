9a413a5da4a8e1427ffa589342e1ec50d2a6a636
John Rua <Jrua89@gmail.com>
2023-10-13 15:19:01 -0400

    layout change using controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
     //show all listing
     public function index() {
-
-
+        return view('listings.index', [
+            'listings' => Listing::all()
+        ]);
     }
 
     //show single listing
-    public function show() {
-    
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
     
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -14,20 +16,11 @@ use App\Models\Listing;
 |
 */
 
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-    ]);
-});
+//All listings
+Route::get('/', [ListingController::class, 'index']);
 
 //Single listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-
-        return view('listing', [
-            'listing' => $listing
-        ]);
-
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
 
